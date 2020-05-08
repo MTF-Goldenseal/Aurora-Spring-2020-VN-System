027513aeec28430c8b25386f2e3d356cbc3ed360
SJCRPV <sjcrpv.games@hotmail.com>
2020-05-08 17:15:59 +0100

    Timer script
    Timer script for the QTE at the end of the dream sequence

---
+++ Assets/Code/DreamStabTimer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class DreamStabTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float timeToPressInput = 10f;
+    private Timer timerToPressInput;
+
+    private void StabThroat()
+    {
+        Debug.Log("Stab");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Timer Started");
+        timerToPressInput = Timer.Register(timeToPressInput, () => StabThroat());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+            {
+                Debug.Log("Cancelled the timer");
+                timerToPressInput.Cancel();
+                StabThroat();
+            }
+        }
+    }
+}
